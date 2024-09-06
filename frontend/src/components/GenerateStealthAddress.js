e3a6929a2cabf1429909ca36f8325e4330ec4926
hazem-dh <hazemdhaou.it@gmail.com>
2024-09-06 22:41:37 +0200

    finished address generation

--- frontend/src/components/GenerateStealthAddress.js
+++ frontend/src/components/GenerateStealthAddress.js
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers, BrowserProvider } from "ethers";
 import { FhenixClient } from "fhenixjs";
-import { CONTRACT_ABI } from "../ABI/ABI";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../ABI/ABI";
 
 function GenerateStealthAddress() {
-  // see if user wants to generate
-  //const [privateKey, setPrivateKey] = useState("");
   const [isCopied, setIsCopied] = useState(false);
   const [walletAddress, setWalletAddress] = useState("0x...");
+  const [Pk, setPk] = useState("");
+  const [store, setStore] = useState(false);
+  const [exportPk, setExportPk] = useState(false);
+  const [disabled, Setdisabled] = useState(true);
   const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (store == false && exportPk == false) {
+      Setdisabled(true);
+    } else {
+      Setdisabled(false);
+    }
+  }, [store, exportPk]);
+
+  const handleStoreChange = (event) => {
+    setStore(event.target.checked);
+    console.log(store);
+  };
+  const handlePkChange = (event) => {
+    setExportPk(event.target.checked);
+  };
+
   // Function to copy user address to clip board
   const copyToClipboard = () => {
     navigator.clipboard
@@ -26,6 +45,7 @@ function GenerateStealthAddress() {
 
   // Function to copy user address to clip board
   const GenerateAddress = async () => {
+    console.log(disabled);
     try {
       // Request account access if needed
       await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -41,9 +61,7 @@ function GenerateStealthAddress() {
       const min = 100000000;
       const max = 9223372036854775807;
       const rand = min + Math.random() * (max - min);
-
       const concatenatedInput = rand + userWallet;
-
       // Hash the concatenated string
       const hash = ethers.keccak256(ethers.toUtf8Bytes(concatenatedInput));
       // Sign the hash using MetaMask
@@ -53,29 +71,29 @@ function GenerateStealthAddress() {
       const derivedPrivateKey = signatureHash.slice(0, 66); // Take the first 32 bytes (64 hex chars + '0x')
       const wallet = new ethers.Wallet(derivedPrivateKey);
       setWalletAddress(wallet.address);
+      //store wallet address if needed
+      if (store) {
+        /* global BigInt */
+        let encrypted = await client.encrypt_uint64(BigInt(rand));
+        // smart contract address , to change later
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          CONTRACT_ABI,
+          signer
+        );
+        const tx = await contract.storewallet(encrypted);
+        await tx.wait(); // Wait for theEtransat to be mined
+      }
 
-      /* global BigInt */
-      console.log(Number(rand));
-      let encrypted = await client.encrypt_uint64(BigInt(rand));
-      // smart contract address , to change later
-      const contractAddress = "0xe4e430285D4E1a42DCC3bBa6BF0a4790040C7624";
-      const contract = new ethers.Contract(
-        contractAddress,
-        CONTRACT_ABI,
-        signer
-      );
-      const tx = await contract.store(encrypted);
-      await tx.wait(); // Wait for the transaction to be mined
-      console.log("Transaction successful");
-      // Set the generated private key and wallet address
-      //setPrivateKey(wallet.privateKey);
-      // console.log(wallet.privateKey);
-      console.log(wallet.address);
+      // Show private key
+      if (exportPk) {
+        setPk("Private key : " + wallet.privateKey);
+      }
+      console.log(Pk);
     } catch (error) {
       console.log(error);
-      setError(
-        "An error occurred while generating the key pair: " + error.message
-      );
+      setError("An error occurred while generating the key pair");
+      console.log(error.message);
     }
 
     try {
@@ -87,8 +105,12 @@ function GenerateStealthAddress() {
   return (
     <div className="  flex flex-col items-center justify-center w-1/3 bg-white text-gray-800 p-6 rounded-lg shadow-black  drop-shadow-lg shadow-lg">
       <h2 className="text-xl font-semibold mb-4">Generate a stealth Address</h2>
-      <div className="flex w-full flex-row">
-        <div className="w-3/4 ml-7 relative">
+      <h4 className="text-l  mb-4">
+        Choose wether to store your wallet or export your private key or why not
+        both !{" "}
+      </h4>
+      <div className="flex w-full flex-row mb-2">
+        <div className="w-1/2  relative">
           <input
             id="wallet-address-input"
             type="text"
@@ -133,23 +155,57 @@ function GenerateStealthAddress() {
             )}
           </button>
         </div>
-        <button
-          className="flex items-center justify-center ml-12 px-8  bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-          onClick={(e) => {
-            GenerateAddress(e);
-          }}
-        >
-          Generate
-        </button>
+
+        <div className="mt-2 ml-6 block min-h-[1.5rem] pl-6">
+          <input
+            className="relative float-left -ml-6 mr-2 mt-1 h-5 w-5 appearance-none rounded-md border-2 border-solid border-neutral-300 outline-none cursor-pointer transition-all duration-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_10px_transparent] before:content-[''] checked:border-blue-500 checked:bg-blue-500 checked:before:opacity-[0.16] checked:after:absolute checked:after:top-[2px] checked:after:left-[6px] checked:after:block checked:after:h-[10px] checked:after:w-[5px] checked:after:rotate-45 checked:after:border-2 checked:after:border-l-0 checked:after:border-t-0 checked:after:border-white hover:border-blue-400 hover:before:opacity-[0.1] hover:before:shadow-[0px_0px_0px_10px_rgba(0,0,0,0.1)] focus:shadow-none focus:ring-2 focus:ring-blue-300 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_10px_rgba(0,0,0,0.1)] checked:focus:before:shadow-[0px_0px_0px_10px_rgba(59,113,202,0.4)] disabled:opacity-50 dark:border-neutral-600 dark:checked:border-blue-500 dark:checked:bg-blue-500 dark:focus:ring-blue-300 dark:focus:before:shadow-[0px_0px_0px_10px_rgba(255,255,255,0.4)]"
+            type="checkbox"
+            value={store}
+            id="Storewallet"
+            onChange={handleStoreChange}
+          />
+          <label
+            className="inline-block  text-neutral-700 hover:cursor-pointer dark:text-neutral-300"
+            htmlFor="Storewallet"
+          >
+            Store wallet
+          </label>
+        </div>
+        <div className="mt-2 ml-6 block min-h-[1.5rem] pl-6">
+          <input
+            className="relative float-left -ml-6 mr-2 mt-1 h-5 w-5 appearance-none rounded-md border-2 border-solid border-neutral-300 outline-none cursor-pointer transition-all duration-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_10px_transparent] before:content-[''] checked:border-blue-500 checked:bg-blue-500 checked:before:opacity-[0.16] checked:after:absolute checked:after:top-[2px] checked:after:left-[6px] checked:after:block checked:after:h-[10px] checked:after:w-[5px] checked:after:rotate-45 checked:after:border-2 checked:after:border-l-0 checked:after:border-t-0 checked:after:border-white hover:border-blue-400 hover:before:opacity-[0.1] hover:before:shadow-[0px_0px_0px_10px_rgba(0,0,0,0.1)] focus:shadow-none focus:ring-2 focus:ring-blue-300 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_10px_rgba(0,0,0,0.1)] checked:focus:before:shadow-[0px_0px_0px_10px_rgba(59,113,202,0.4)] disabled:opacity-50 dark:border-neutral-600 dark:checked:border-blue-500 dark:checked:bg-blue-500 dark:focus:ring-blue-300 dark:focus:before:shadow-[0px_0px_0px_10px_rgba(255,255,255,0.4)]"
+            type="checkbox"
+            value={exportPk}
+            onChange={handlePkChange}
+            id="export"
+          />
+          <label
+            className="inline-block pl-2 text-neutral-700 hover:cursor-pointer dark:text-neutral-300"
+            htmlFor="export"
+          >
+            Export private Key
+          </label>
+        </div>
       </div>
-      {/*       <button
-        className="flex items-center justify-center mt-3 px-4 py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+      {exportPk && (
+        <div className="text-red-500 text-shadow-8 font-bold mt-2"> {Pk} </div>
+      )}
+      <button
+        className={`flex items-center justify-center mt-5 py-2 px-8 rounded-lg shadow-md text-white focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 ${
+          disabled
+            ? "bg-blue-300 cursor-not-allowed opacity-50"
+            : "bg-blue-600 hover:bg-blue-500"
+        }`}
         onClick={(e) => {
-          onButtonclick(e);
+          GenerateAddress(e);
         }}
+        disabled={disabled}
       >
-        Save
-      </button> */}
+        Generate
+      </button>
+      {error && (
+        <div className="text-red-500 text-shadow-8 font-bold ">{error}</div>
+      )}
     </div>
   );
 }
